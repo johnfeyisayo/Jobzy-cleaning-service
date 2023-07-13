a29aa6a6df0f1daf6633c4c64869ab78b00ed5d3
john feyisayo <johnfeyisayo@gmail.com>
2023-07-13 15:38:51 +0100

    create branch

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Booking from './components/Booking';
+import Navbar from './components/Navbar';
+import Contact from './components/Contact';
+import Services from './components/Services';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='booking' element={<Booking />}></Route>
+      <Route path='navbar' element={<Navbar />}></Route>
+      <Route path='contact' element={<Contact />}></Route>
+      <Route path='services' element={<Services />}></Route>
+    </Routes>
+    <p className="h-10 w-10 rounded-full">Hello</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <h1>Hi you&apos;re on the Navbar page</h1>
+    </div>
+  )
+}
+
+export default Navbar
